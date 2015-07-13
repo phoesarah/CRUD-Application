c92107a280d0234ba13747fedb02b170a8594fc2
Sarah R <phoesarah@hotmail.com>
2015-07-12 22:05:48 -0500

    Cleaned up comments

--- GorillaChimpDataJamWF/Form1.cs
+++ GorillaChimpDataJamWF/Form1.cs
@@ -12,9 +12,7 @@ namespace GorillaChimpDataJamWF
 {
     public partial class Form1 : Form
     {
-        //private dictionary object string string selected search fields  then set = to new on form load.  Then attach cchange events to all of those fields that
-        //check for existence of key and if does not exist add key and value, else update value.  key is name of column; value is value and throw in there. 
-        //Dataset not bound. 
+        
         private Dictionary<string, string> selectedsearchfields;
         Gorillas2Entities1 db = new Gorillas2Entities1();
         DataGridViewLinkColumn Deletelink = new DataGridViewLinkColumn();
@@ -73,9 +71,7 @@ namespace GorillaChimpDataJamWF
                 selectedsearchfields.Remove("Observer");
                 selectedsearchfields.Add("Observer", ((ComboBox)sender).Text);
                 
-               
-                
-                // selectedsearchfields["Observer"] = ((ComboBox)sender).SelectedItem.ToString();
+
             }
             else
             {
@@ -236,8 +232,7 @@ namespace GorillaChimpDataJamWF
             {
                 MessageBox.Show("Sorry, no results match your query");
             }
-            //dataGridView1.DataSource = db.HabEncounters.Where(x => x.SPECIES == "gorilla").ToList();
-            //selectedsearchfields["Observer"].ToString()).ToList();
+  
         }
 
         private void habEncounterBindingSource_CurrentChanged(object sender, EventArgs e)
